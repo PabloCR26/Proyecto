aecaf36954b9619098064a1c7347f77f3889de70
PabloCR26 <chavespablo2605@gmail.com>
2023-07-21 20:13:35 -0600

    Creacion de la clase libro

---
+++ libro.py
@@ -0,0 +1,8 @@
+class Libro:
+    def __init__(self,titulo,autor,year,genero,descripcion):
+        self.titulo=titulo
+        self.autor=autor
+        self.year=year
+        self.genero=genero
+        self.descripcion=descripcion
+        
