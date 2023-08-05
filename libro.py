6b0d01b25b1538192113cdb8e8ecd7ed1cdc12a9
PabloCR26 <chavespablo2605@gmail.com>
2023-08-04 21:40:49 -0600

    Se crearon funciones de devolver y prestar libro

--- libro.py
+++ libro.py
@@ -1,24 +1,26 @@
 import uuid
 
 
-
 class Libro:
 
-    def __init__(self,titulo,autor,year,genero,descripcion):
-        self.id= uuid.uuid4()
-        self.titulo=titulo
-        self.autor=autor
-        self.year=year
-        self.genero=genero
-        self.descripcion=descripcion
+    def __init__(self, titulo, autor, year, genero, descripcion):
+        self.id = uuid.uuid4()
+        self.titulo = titulo
+        self.autor = autor
+        self.year = year
+        self.genero = genero
+        self.descripcion = descripcion
+        self.disponible = True
+        self.reservado = False
+        self.inicio_prestamo = ""
+        self.fin_prestamo = ""
 
+    def prestar(self, fecha_inicio, fecha_fin):
+        self.disponible = False
+        self.inicio_prestamo = fecha_inicio
+        self.fin_prestamo = fecha_fin
 
-
-
-
-    '''Pseudocodigo'''
-    #1. Solicitar título del libro al usuario.
-    #2. Buscar el título en la lista de libros.
-    #3. Imprimir el libro si se encuentra.
-
-
+    def devolver(self):
+        self.disponible = True
+        self.inicio_prestamo = ""
+        self.fin_prestamo = ""
--- main.py
+++ main.py
@@ -2,102 +2,106 @@ from libro import Libro
 from usuario import Usuario
 from empleado import Empleado
 
+lista_de_usuarios = []
+lista_de_empleados = []
+lista_de_libros = []
 
-lista_de_usuarios=[]
-lista_de_empleados=[]
-lista_de_libros=[]
+
 def menu_ingreso():
-
     while True:
-        opcion = int(input(' Menu\n----------\t'+
-                       '\n\t1. Añadir nuevo libro'
-                       '\n\t2. Ver lista de libros'
-                       '\n\t3. Buscar libro por título'
-                       '\n\t4. Buscar libro por ID'
-                        '\n\t5. Buscar libro por Autor'
-                        '\n\t6. Buscar libro por año'
-                        '\n\t7. Buscar libro por Genero'
-                       '\n\t8. Inscribir usuario'
-                       '\n\t9. Inscribir empleado'
-                       '\n\t10. Eliminar libros'
-                       '\n\t11. Reservar libro'
-                       '\n\t12. Filtrar libros'
-                       '\n\t13. Modificar atributos del libro'
-                       '\n\t14. Prestamo de libro'
-                       '\n\t15. Devolución de libro'
-                       '\n\t16. Generar informe'
-                       '\n\t17. Salir del sistema'
-                       '\nIngrese la opcion que desea:\n'))
+        opcion = int(input(' Menu\n----------\t' +
+                           '\n\t1. Añadir nuevo libro'
+                           '\n\t2. Ver lista de libros'
+                           '\n\t3. Buscar libro por título'
+                           '\n\t4. Buscar libro por ID'
+                           '\n\t5. Buscar libro por Autor'
+                           '\n\t6. Buscar libro por año'
+                           '\n\t7. Buscar libro por Genero'
+                           '\n\t8. Inscribir usuario'
+                           '\n\t9. Inscribir empleado'
+                           '\n\t10. Eliminar libros'
+                           '\n\t11. Reservar libro'
+                           '\n\t12. Filtrar libros'
+                           '\n\t13. Modificar atributos del libro'
+                           '\n\t14. Prestamo de libro'
+                           '\n\t15. Devolución de libro'
+                           '\n\t16. Generar informe'
+                           '\n\t17. Salir del sistema'
+                           '\nIngrese la opcion que desea:\n'))
 
-        if opcion ==1:
-           anadir_libro()
-        elif opcion ==2:
+        if opcion == 1:
+            anadir_libro()
+        elif opcion == 2:
             mostrar_lista_libros()
-        elif opcion ==3:
+        elif opcion == 3:
             buscar_libro("titulo")
-        elif opcion ==4:
+        elif opcion == 4:
             buscar_libro("id")
-        elif opcion== 5:
+        elif opcion == 5:
             buscar_libro("autor")
-        elif opcion== 6:
+        elif opcion == 6:
             buscar_libro("year")
         elif opcion == 7:
             buscar_libro("genero")
-        elif opcion ==8:
+        elif opcion == 8:
             crear_usuario()
             print('Usuario inscrito')
-        elif opcion ==9:
+        elif opcion == 9:
             cargar_empleados()
-        elif opcion ==10:
+        elif opcion == 10:
             eliminar_libro()
-        elif opcion ==11:
+        elif opcion == 11:
             print('Libro reservado')
-        elif opcion ==12:
+        elif opcion == 12:
             print('Atributos modificados')
-        elif opcion ==13:
-            print('Atributos modificados')
-        elif opcion ==14:
-            print('Préstamo realizado')
-        elif opcion ==15:
-            print('Libro devuelto')
-        elif opcion ==16:
+        elif opcion == 13:
+            modificar_atributos_libro()
+        elif opcion == 14:
+            prestamo_libro()
+        elif opcion == 15:
+            devolucion_libro()
+        elif opcion == 16:
             print('Informe generado')
-        elif opcion==17:
+        elif opcion == 17:
             break
         else:
             print('¡Opción no valida!')
 
 
 def crear_usuario():
-    cedula=input("Ingrese su numero de cedula: ")
-    nombre=input("Ingrese su nombre: ")
-    apellido1=input("Ingrese su primer apellido: ")
-    apellido2=input("Ingrese su segundo apellido: ")
-    direccion=input("Ingrese su direccion: ")
-    telefono=input("Ingrese su numero de telefono: ")
+    cedula = input("Ingrese su numero de cedula: ")
+    nombre = input("Ingrese su nombre: ")
+    apellido1 = input("Ingrese su primer apellido: ")
+    apellido2 = input("Ingrese su segundo apellido: ")
+    direccion = input("Ingrese su direccion: ")
+    telefono = input("Ingrese su numero de telefono: ")
 
-    objUsuario=Usuario(cedula,nombre,apellido1,apellido2,direccion,telefono)
+    objUsuario = Usuario(cedula, nombre, apellido1, apellido2, direccion, telefono)
     lista_de_usuarios.append(objUsuario)
-    print("Usuario: "+ nombre + " agregado correctamente.")
+    print("Usuario: " + nombre + " agregado correctamente.")
+
 
 def cargar_empleados():
     with open('empleados.txt', 'r') as f:
         for linea in f:
-            datos=linea.split(',')
+            datos = linea.split(',')
             obj_empleado = Empleado(datos[0], datos[2], datos[3], datos[4], datos[1], datos[5])
             lista_de_empleados.append(obj_empleado)
             print('El empleado ' + obj_empleado.nombre + ' fue agregado correctamente')
 
-def anadir_libro():
-    titulo=input("Ingrese el titulo del libro: ")
-    autor=input("Ingrese el nombre del autor: ")
-    year=input("Ingrese el año de creacion: ")
-    genero=input("Ingrese el genero: ")
-    descripcion=input("Ingrese una breve descripcion del libro: ")
 
-    objLibro=Libro(titulo,autor,year,genero,descripcion)
+def anadir_libro():
+    titulo = input("Ingrese el titulo del libro: ")
+    autor = input("Ingrese el nombre del autor: ")
+    year = input("Ingrese el año de creacion: ")
+    genero = input("Ingrese el genero: ")
+    descripcion = input("Ingrese una breve descripcion del libro: ")
+
+    objLibro = Libro(titulo, autor, year, genero, descripcion)
     lista_de_libros.append(objLibro)
-    print("El libro " + titulo + " fue agregado correctamente. ")
+
+    print("El libro " + titulo + " con el ID " + str(objLibro.id) + " fue agregado correctamente. ")
+
 
 def eliminar_libro():
     id = input("Ingrese el ID del libro: ")
@@ -107,16 +111,16 @@ def eliminar_libro():
             print('El libro ' + id + ' ha sido eliminado.')
 
 
-
 def buscar_libro(propiedad_buscar):
-
     valor = input(f"Ingrese el valor de la propiedad {propiedad_buscar} del libro: ")
     for libro in lista_de_libros:
-        if (str(getattr(libro,propiedad_buscar))==valor):
+        if (str(getattr(libro, propiedad_buscar)) == valor):
             print(" ID: " + str(libro.id) + " \n Titulo: " + libro.titulo + "\n Autor: " + libro.autor
                   + "\n Genero: " + libro.genero + "\n Descripcion: " + libro.descripcion + "\n Año: " + libro.year)
 
+
 def modificar_atributos_libro():
+    id = input("Ingrese el ID del libro: ")
     titulo = input("Ingrese el titulo del libro: ")
     autor = input("Ingrese el nombre del autor: ")
     year = input("Ingrese el año de creacion: ")
@@ -124,18 +128,47 @@ def modificar_atributos_libro():
     descripcion = input("Ingrese una breve descripcion del libro: ")
 
     for libro in lista_de_libros:
-        if libro.titulo == titulo:
+        if str(libro.id) == id:
+            if (titulo != ""):
+                libro.titulo = titulo
             if (autor != ""):
                 libro.autor = autor
             if (year != ""):
                 libro.year = year
+            if (genero != ""):
+                libro.genero = genero
+            if (descripcion != ""):
+                libro.descripcion = descripcion
+            print(" Los atributos han sido modificados!!! ")
+
 
 def mostrar_lista_libros():
     for libro in lista_de_libros:
-        print(" ID: "+ str(libro.id) + " \n Titulo: " + libro.titulo+ "\n Autor: "+ libro.autor
-              + "\n Genero: "+ libro.genero + "\n Descripcion: "+ libro.descripcion+ "\n Año: "+ libro.year)
+        print(" ID: " + str(libro.id) + " \n Titulo: " + libro.titulo + "\n Autor: " + libro.autor
+              + "\n Genero: " + libro.genero + "\n Descripcion: " + libro.descripcion + "\n Año: " + libro.year)
 
 
+def prestamo_libro():
+    id = input("Ingrese el ID del libro que desea solicitar: ")
+    fecha_prestamo = input("Ingrese fecha de inicio de prestamo del libro: ")
+    fecha_devolucion = input("Ingrese fecha de devolucion del libro: ")
+    for libro in lista_de_libros:
+        if str(libro.id) == id:
+            if libro.disponible:
+                libro.prestar(fecha_prestamo, fecha_devolucion)
+                print("Su libro ha sido prestado correctamente")
+            else:
+                print("Su libro no se encuentra disponible!!")
+
+
+def devolucion_libro():
+    id = input("Ingrese el ID del libro que desea devolver : ")
+    for libro in lista_de_libros:
+        if str(libro.id) == id:
+            if not libro.disponible:
+                libro.devolver()
+            else:
+                print("Su libro no se encuentra disponible!!")
 
 
 # Press the green button in the gutter to run the script.
